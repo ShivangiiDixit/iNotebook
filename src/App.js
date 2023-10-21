5bf51584f81237d7b217221740f7519c2fe202c2
Shivangi Dixit <shivangigovind@example.com>
2023-10-22 00:38:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NAVBAR from './Component/NAVBAR';
+
+let name = "<b>harry3</b>";
+function App() {
+  return (
+    <>
+    <NAVBAR/>
+    </>
+  );
+
+  
+    
+}
+
+export default App;
